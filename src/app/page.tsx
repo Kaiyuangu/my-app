6e138c42eaff80c1466e4ac2719ceced99d8a0d9
Kaiyuangu <706827331@qq.com>
2024-07-31 21:10:08 +0800

    feat: moon  setInterver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
-'use client'
+"use client";
 import Image from "next/image";
-import {useState} from "react";
+import { useState } from "react";
 export default function Home() {
-const moons=["🌑","🌒","🌓","🌔","🌕","🌖","🌗","🌘","🌑"];
-const [index,setI]=useState(0);
-  function showAlert(){
-  setI((index+1)%moons.length);
-}
+  const moons = ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘", "🌑"];
+  const [index, setIndex] = useState(0);
+  function nextIndex() {
+    setInterval(() => {
+      setIndex((oldIndex) => {
+        return (oldIndex + 1) % moons.length;
+      });
+    }, 500);
+  }
   return (
-   <main>
-      <div className="text-xl italic">
-        {moons[index]}
-      </div>
-      <button onClick={showAlert} className="border rounded-md hover:bg-slate-100 p-1">
+    <main>
+      <div className="text-xl italic">{moons[index]}</div>
+      <button
+        onClick={nextIndex}
+        className="border rounded-md hover:bg-slate-100 p-1"
+      >
         按钮
       </button>
-  </main>
+    </main>
   );
 }
