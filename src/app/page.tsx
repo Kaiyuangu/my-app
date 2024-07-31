910817d92ab4098dcc39f6dfb1b9a47395055ce6
Kaiyuangu <706827331@qq.com>
2024-07-31 19:26:14 +0800

    feat: moons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+'use client'
 import Image from "next/image";
-
+import {useState} from "react";
 export default function Home() {
+const moons=["🌑","🌒","🌓","🌔","🌕","🌖","🌗","🌘","🌑"];
+const [index,setI]=useState(0);
+  function showAlert(){
+  setI((index+1)%moons.length);
+}
   return (
    <main>
       <div className="text-xl italic">
-        hello 
+        {moons[index]}
       </div>
+      <button onClick={showAlert} className="border rounded-md hover:bg-slate-100 p-1">
+        按钮
+      </button>
   </main>
   );
 }
