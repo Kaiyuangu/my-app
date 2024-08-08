366bbc4a9e6fc4901ed0b8f4e5b22b800982f52c
Kaiyuangu <706827331@qq.com>
2024-08-08 20:19:24 +0800

    feat: add confetti

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,18 @@
 'use client'
 import { useState } from "react";
-import {useWindowSize} from 'react-use'
-import Confetti from 'react-confetti'
+import {useWindowSize} from 'react-use';
+import Confetti from 'react-confetti';
+import Countdown from "react-countdown";
 export default function Home() {
-  let[counter,setCounter]=useState(0);
   const { width, height } = useWindowSize()
-  function handleClick(){
-    setCounter(counter+1);
-    console.log(counter);
-  }
+  
   return (
-    <main className="flex items-center w-sccreen h-screen">
-      {
-        (counter>3)&&<Confetti width={width} height={height} wind={-0.09}/>
-        
-      }
-      <div className="m-auto">
-        <div >
-          i have clicked {counter} time.
+    <main >
+      <div className="h-screen w-screen flex">
+            <Countdown date={Date.now()+5000} className="m-auto text-6xl font-extralight">
+            <Confetti width={width}height={height} recycle={false} gravity={0.2} wind={-0.1} numberOfPieces={400}/>
+            </Countdown>
       </div>
-      <div>
-        <button onClick={handleClick} className="border rounded-lg p-2 bg-sky-400 text-white hover:bg-sky-600 active:bg-sky-800">
-          Click Me!
-        </button>
-      </div>
-      </div>
-      
     </main>
   );
 }
