4d5ddf8d803c035e815f94fd86bd9222f0d087d0
Kaiyuangu <706827331@qq.com>
2024-08-17 17:33:25 +0800

    feat: Filter`todos`

--- src/app/components/Today.tsx
+++ src/app/components/Today.tsx
@@ -5,14 +5,38 @@ interface TodatArgs{
 }
 
 function Today({todos}:TodatArgs){
+  const todosToday=todos.filter((todo)=>{
+    const dateTodo=new Date(todo.getExpireDate());
+    const dateToday=new Date(Date.now());
+    if(dateTodo.toDateString()===dateToday.toDateString()){
+      return todo;
+    }
+  })
+  const todosTomorrow=todos.filter((todo)=>{
+    const dateTodo=new Date(todo.getExpireDate());
+    const dateTomorrow=new Date(Date.now());
+    dateTomorrow.setDate(dateTomorrow.getDate()+1);
+    if(dateTodo.toDateString()===dateTomorrow.toDateString()){
+      return todo;
+    }
+  })
+  const todosLater=todos.filter((todo)=>{
+    const dateTodo=new Date(todo.getExpireDate());
+    const dateLater=new Date(Date.now());
+    dateLater.setDate(dateLater.getDate()+2);
+    if(dateTodo.toDateString()===dateLater.toDateString()){
+      return todo;
+    }
+  })
   return(
     <div className="flex-1 p-4" >
     <h1 className=" text-blue-600 text-4xl font-bold mb-6">最近</h1>
+    
     <div className="border rounded-lg p-6 bg-white flex h-5/6 rounded-xl shadow-md divide-dashed divide-x">
      <div className="w-1/3 text-2xl">
       今天
       {
-        todos.map((todo,index)=>{
+        todosToday.map((todo,index)=>{
           return(
             <TodoComponent key={todo.getId()}title={todo.getTitle()}expireDate={todo.getExpireDate()} 
             status={todo.getStatus()}
@@ -20,11 +44,31 @@ function Today({todos}:TodatArgs){
           );
         })
       }
-      <div>
-        <div className="w-1/3 text-2xl">明天</div>
-        <div className="w-1/3 text-2xl">后天·</div>
-      </div>
      </div>
+     <div className="w-1/3 text-2xl">
+      明天
+      {
+        todosTomorrow.map((todo,index)=>{
+          return(
+            <TodoComponent key={todo.getId()}title={todo.getTitle()}expireDate={todo.getExpireDate()} 
+            status={todo.getStatus()}
+            />
+          );
+        })
+      }
+      </div>
+      <div className="w-1/3 text-2xl">
+        后天
+        {
+        todosLater.map((todo,index)=>{
+          return(
+            <TodoComponent key={todo.getId()}title={todo.getTitle()}expireDate={todo.getExpireDate()} 
+            status={todo.getStatus()}
+            />
+          );
+        })
+      }
+        </div>
     </div>
   </div>
   );
--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -6,11 +6,13 @@ interface TodoArgs{
 }
 function convertTimestampToDateString(expireDate:number){
     let date=new Date(expireDate);
-    return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+    console.log(expireDate);
+    //return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
+    return date.toDateString();
 }
 function Todo({title,expireDate,status}:TodoArgs){
     return (<div className="border ruturned-md shadow-md
-    bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-while font-bold p-3 hover:opacity-80">
+    bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-while font-bold rounded-lg p-3 hover:opacity-80 text-white">
         <div>
             {title}
         </div>
