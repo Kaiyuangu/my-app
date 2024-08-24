fc7cfdc98e5d3079227b60c221ab0ccd31f978a9
Kaiyuangu <706827331@qq.com>
2024-08-24 16:40:12 +0800

    feat: implement Finished component

--- src/app/components/Finished.tsx
+++ src/app/components/Finished.tsx
@@ -1,16 +1,29 @@
+import { TodoStatus } from "../Todo";
 import { Todo } from "../Todo";
-
+import { Todo as TodoComponent } from "./Todo";
 interface FinishedArgs {
-    todos: Todo[],
+    todos:Todo[],
     setTodo: (todo: Todo) => void
 }
 
-function Finished({todos, setTodo}: FinishedArgs) {
-    return (
-        <div>
-            Finished
-        </div>
-    );
+function Finished({ todos, setTodo }: FinishedArgs) {
+  const FinishedTodos = todos.filter(todo=>todo.getStatus()===TodoStatus.FINISHED);
+  return (
+      <div className="flex-1 p-4">
+          <h1 className="text-red-600 text-4xl font-bold mb-6"> 已完成 </h1>
+          <div className="bg-white grid grid-cols-4 gap-4 h-5/6 rounded-xl shadow-md overflow-y-auto p-4">
+              {
+                  todos.map((todo) => {
+                      return <TodoComponent key={todo.getId()} title={todo.getTitle()} expireDate={todo.getExpireDate()}
+                          status={todo.getStatus()}
+                          id={todo.getId()}
+                          setTodo={setTodo}
+                      />
+                  })
+              }
+          </div>
+      </div>
+  );
 }
 
 export {Finished};
